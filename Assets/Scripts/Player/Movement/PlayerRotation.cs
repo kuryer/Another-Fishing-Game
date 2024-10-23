72c73850857e4a9074f68755cca1808addf4ecd3
kuryer <kuryergdev@gmail.com>
2024-10-23 20:56:23 +0200

    init 2

---
+++ Assets/Scripts/Player/Movement/PlayerRotation.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+
+public class PlayerRotation : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] Rigidbody rb;
+    [SerializeField] Transform Orientation;
+    [SerializeField] Transform MovementReferenceObject;
+
+    [Header("Walking Parameters")]
+    [SerializeField] float walkingSpeed;
+    [SerializeField] float rotationSpeed;
+    Vector2 inputDirection;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+
+    void Move()
+    {
+        Vector3 viewDir = transform.position - new Vector3(MovementReferenceObject.position.x, transform.position.y, MovementReferenceObject.position.z);
+        Orientation.transform.forward = viewDir.normalized;
+
+        Vector3 moveDir = Orientation.forward * inputDirection.y + Orientation.right * inputDirection.x;
+
+        if (moveDir != Vector3.zero)
+            transform.parent.forward = Vector3.Slerp(transform.parent.forward, moveDir.normalized, Time.deltaTime * rotationSpeed);
+    }
+
+    public void SetMoveDirection(InputAction.CallbackContext context)
+    {
+        if (!context.performed)
+            return;
+
+        inputDirection = context.ReadValue<Vector2>();
+    }
+}
