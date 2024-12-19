76f94f56513e99114ce8bdd35650eac183ab22d4
Paanwel <paanwel123@gmail.com>
2024-12-19 18:07:45 +0100

    e er

--- Assets/Scripts/Player/Fishing Bob/Bob.cs
+++ Assets/Scripts/Player/Fishing Bob/Bob.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Bob : MonoBehaviour
 {
-    [SerializeField] AnimationCurve throwTrajectory;
+    [SerializeField] AnimationCurve throwTrajectoryX;
+    [SerializeField] AnimationCurve throwTrajectoryY;
+    [SerializeField] float debugValue;
     [SerializeField] float bobSpeed;
     [SerializeField] Rigidbody bobRB;
     [SerializeField] FishingDetection detection;
     [SerializeField] string waterTag;
     Coroutine throwCoroutine;
+    Vector3 throwDirection;
     void Start()
     {
         
@@ -19,27 +23,43 @@ public class Bob : MonoBehaviour
         
     }
 
-    public void ThrowBob()
+    public void ThrowBob(InputAction.CallbackContext context)
     {
-        throwCoroutine = StartCoroutine(BobTravel());
+        if(context.performed)
+            throwCoroutine = StartCoroutine(BobTravel());
     }
 
     IEnumerator BobTravel()
     {
         Vector2 distanceInfo = detection.GetDistance();
-        float currentDistance = distanceInfo.x;
+        float currentDistance = 0;
         float maxDistance = distanceInfo.x + distanceInfo.y;
+        throwDirection = bobRB.transform.forward.normalized;
+        Vector3 startingPos = bobRB.position;
+        Debug.Log("BobTravel");
         while (enabled)
         {
-            bobRB.position = bobRB.transform.forward * throwTrajectory.Evaluate(currentDistance / maxDistance);
+            bobRB.position = startingPos + maxDistance * EvaluateTrajectory(currentDistance / maxDistance);
             currentDistance += bobSpeed * Time.deltaTime;
             yield return null;
         }
     }
 
+    private Vector3 EvaluateTrajectory(float time)
+    {
+        float x = throwTrajectoryX.Evaluate(time) * throwDirection.x;
+        float z = throwTrajectoryX.Evaluate(time) * throwDirection.z;
+        float y = throwTrajectoryY.Evaluate(time);
+        Debug.Log(time);
+        return new Vector3(x, y, z);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag(waterTag))
-            StopCoroutine(throwCoroutine);
+        if (other.CompareTag(waterTag))
+        {
+            Debug.Log("StopCoroutine");
+            StopCoroutine(throwCoroutine);  
+        }
     }
 }
